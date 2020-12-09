9c215fe042ff041cd268290afa4cda7514f5560c
lthUniBonn <lthimm@uni-bonn.de>
2020-12-09 13:27:20 +0100

    Downloading specifics for fionas files -> need all corresponding stc

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ Attributes:
 
 """
 # General settings.
-start_year = 2019#2011
-final_year = 2019#2017
+start_year = 2019
+final_year = 2019
 era5_data_dir = '/cephfs/user/s6lathim/ERA5Data/'
 model_level_file_name_format = "{:d}_europe_{:d}_130_131_132_133_135.nc"  # 'ml_{:d}_{:02d}.netcdf'
 surface_file_name_format = 'sfc_{:d}_{:02d}.netcdf'  #"{:d}_europe_{:d}_152.nc"
