230ec0de5c394b94842bfe633157fe7a07d5fd82
lthUniBonn <lthimm@uni-bonn.de>
2021-01-24 20:25:11 +0100

    plotting user input in separate function

--- plot_maps.py
+++ plot_maps.py
@@ -4,7 +4,7 @@
 
 Example::
 
-    $ python plot_maps.py              : plot from files with maximal subset id of the preset value
+    $ python plot_maps.py -c           : plot from files from combined output file
     $ python plot_maps.py -m max_id    : plot from files with maximal subset id of max_id
     $ python plot_maps.py -h           : display this help
 
@@ -35,37 +35,8 @@ n_fill_levels_default = 14
 n_line_levels_default = 6
 color_map = cm.YlOrRd
 
-# Load the processed data from the NetCDF file.
-help = """
-    python plot_maps.py -c           : plot from files from combined output file
-    python plot_maps.py -m max_id    : plot from files with maximal subset id of max_id
-    python plot_maps.py -h           : display this help
-    """.format(max_subset_id)
-
-if len(sys.argv) > 1: 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hm:c", ["help", "maxid=", "combined"])
-    except getopt.GetoptError:     # User input not given correctly, display help and end
-        print(help)
-        sys.exit()
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):    # Help argument called, display help and end
-            print (help)
-            sys.exit()
-        elif opt in ("-m", "--maxid"):     # User Input maximal subset id given  
-            max_subset_id = int(arg)
-            # find all subset files matching the settings in config.py - including all until max_subset_id 
-            all_year_subset_files = [output_file_name_subset.format(**{'start_year':start_year, 'final_year':final_year,\
-                'lat_subset_id':subset_id, 'max_lat_subset_id':max_subset_id}) for subset_id in range(max_subset_id +1)]
-            print('All data for the years {} to {} is read from subset_files from 0 to {}'.format(start_year,\
-                final_year, max_subset_id))
-            nc = xr.open_mfdataset(all_year_subset_files, concat_dim='latitude')
-        elif opt in ("-c", "--combined"):     # User Input to use combined file
-            file_name = output_file_name.format(**{'start_year':start_year, 'final_year':final_year})
-            nc = xr.open_dataset(file_name)
-else:
-    print(help)
-
+# Load the processed data from the NetCDF files specified in the input.
+nc = read_dataset_user_input()
 
 lons = nc['longitude'].values
 lats = nc['latitude'].values
@@ -85,6 +56,46 @@ map_plot = Basemap(projection='merc', llcrnrlon=np.min(lons), llcrnrlat=np.min(l
                    urcrnrlat=np.max(lats), resolution=map_resolution)
 x_grid, y_grid = map_plot(lons_grid, lats_grid)  # Compute map projection coordinates.
 map_plot_aspect_ratio = 9. / 12.3  # Aspect ratio of Europe map.
+
+def read_dataset_user_input():
+    """"Interpret user input to open the dataset(s)
+
+    Returns:
+        nc (Dataset): Plotting input data (from processing)
+    """
+
+    help = """
+        python plot_maps.py -c           : plot from files from combined output file
+        python plot_maps.py -m max_id    : plot from files with maximal subset id of max_id
+        python plot_maps.py -h           : display this help
+        """
+
+    if len(sys.argv) > 1: 
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], "hm:c", ["help", "maxid=", "combined"])
+        except getopt.GetoptError:     # User input not given correctly, display help and end
+            print(help)
+            sys.exit()
+        for opt, arg in opts:
+            if opt in ("-h", "--help"):    # Help argument called, display help and end
+                print (help)
+                sys.exit()
+            elif opt in ("-m", "--maxid"):     # User Input maximal subset id given  
+                max_subset_id = int(arg)
+                # find all subset files matching the settings in config.py - including all until max_subset_id 
+                all_year_subset_files = [output_file_name_subset.format(**{'start_year':start_year, 'final_year':final_year,\
+                    'lat_subset_id':subset_id, 'max_lat_subset_id':max_subset_id}) for subset_id in range(max_subset_id +1)]
+                print('All data for the years {} to {} is read from subset_files from 0 to {}'.format(start_year,\
+                    final_year, max_subset_id))
+                nc = xr.open_mfdataset(all_year_subset_files, concat_dim='latitude')
+            elif opt in ("-c", "--combined"):     # User Input to use combined file
+                file_name = output_file_name.format(**{'start_year':start_year, 'final_year':final_year})
+                nc = xr.open_dataset(file_name)
+    else:
+        print(help)
+        sys.exit()
+
+    return nc
 
 
 def calc_fig_height(fig_width, subplot_shape, plot_frame_top, plot_frame_bottom, plot_frame_left, plot_frame_right):
